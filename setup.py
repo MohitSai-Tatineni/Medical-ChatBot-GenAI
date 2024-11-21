6038db9bf2bd9336583ef3cc3e928ca531128d27
Dhyey Joshi <dhyey1974@gmail.com>
2024-11-21 13:17:50 -0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CarePal',
+    version= '0.0.0',
+    author= 'Dhyey Joshi',
+    author_email= 'dhyeyjoshi12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
